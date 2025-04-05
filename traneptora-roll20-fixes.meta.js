f5e09c0f6aea1ed65e3746ea099c68295fe680da
Leo Izen <leo.izen@gmail.com>
2025-04-05 02:36:03 -0400

    commit: 2025-04-05T06:36:03+00:00

---
+++ traneptora-roll20-fixes.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Traneptora's Roll20 Cleanup Script
+// @namespace    https://traneptora.com/
+// @version      2025.04.05.3
+// @updateURL    https://raw.githubusercontent.com/Traneptora/roll20-cleanup/refs/heads/dist/traneptora-roll20-fixes.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Traneptora/roll20-cleanup/refs/heads/dist/traneptora-roll20-fixes.user.js
+// @description  Traneptora's Roll20 Cleanup Script
+// @author       Traneptora
+
+// @match        https://app.roll20.net/editor
+// @match        https://app.roll20.net/editor#*
+// @match        https://app.roll20.net/editor?*
+// @match        https://app.roll20.net/editor/
+// @match        https://app.roll20.net/editor/#*
+// @match        https://app.roll20.net/editor/?*
+
+// @run-at       context-menu
+
+// @grant        none
+// ==/UserScript==
